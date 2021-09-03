3b2080b6a4d366efdcd359be6f84a17066e9c675
Paul Toal <paul.toal@gmail.com>
2021-09-03 12:19:21 +0100

    Update 5b-DeployingWordpress-SH.sh

--- 5b-DeployingWordpress-SH.sh
+++ 5b-DeployingWordpress-SH.sh
@@ -3,8 +3,12 @@
 echo Initialising the Docker swarm
 docker swarm init
 
-echo Check we are in the wordpress folder
+echo Creating a directory for Wordpress files
+mkdir /home/opc/wordpress
 cd /home/opc/wordpress
+
+echo Downloading the Wordpress stack file
+wget https://github.com/pdtoal/CloudSecWorkshopScripts/blob/main/wordpress-stack.yml
 
 echo Deploying the Wordpress stack
 docker stack deploy -c wordpress-stack.yml wordpress
